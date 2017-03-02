9eeb65410f1fec344f60546557a25f5eeb88f020
kaka03 <381633032@qq.com>
2017-03-03 00:54:29 +0800

    2017/03/03

--- validate/vendor/common.js
+++ validate/vendor/common.js
@@ -1,78 +1,68 @@
-window.onload=function(){
-		var oBtn=document.getElementById("btnSignup");
+$(document).ready(function () {
+		
 			//点击登录按钮
-			oBtn.onclick=function(){
+	$("#btnSignup").click(function(){
 				openNew();
-				return false;
-				}
-}
+    });
 	function openNew(){
 		//获取页面的高度和宽度
-		var sWidth=document.body.scrollWidth;
-		var sHeight=document.body.scrollHeight;
+		var sWidth=$(document).width();
+		var sHeight=$(document).height();
 		//获取页面的可视区域高度和宽度
-		var wHeight=document.documentElement.clientHeight;
-		var oMask=document.createElement("div");
-			oMask.id="mask";
-			oMask.style.height=sHeight+"px";
-			oMask.style.width=sWidth+"px";
-			document.body.appendChild(oMask);
-		var oSignup=document.createElement("div");
-			oSignup.id="signup";
-			oSignup.innerHTML="<div class='signupCon'>\
-			<div id='signupheader'>\
-			<div id='close'>点击关闭</div>\
-			</div>\
-			<form id='demoForm'>\
-    		<fieldset>\
-        		<legend align='center'>用户注册</legend>\
-        		<p id='info'></p>\
-        		<p id='info2' style='display: none'>输入错误</p>\
-       			<p>\
-       			<p>\
-            	<label for='username'>用户名</label>\
-            	<input type='text' id='username' name='username'/>\
-        		</p>\
-        		<p>\
-           			<label for='password'>密码</label>\
-           			<input type='password' id='password' name='password'/>\
-        		</p>\
-        		<p>\
-            		<label for='confirm-password'>确认密码</label>\
-            		<input type='password' id='confirm-password' name='confirm-password'/>\
-        		</p>\
-        		<p>\
-            	<label for='email'>邮箱</label>\
-            	<input type='email' id='email' name='email'/>\
-        		</p>\
-        		<p>\
-            	<label for='text'>手机号码</label>\
-            	<input type='text' id='mobile' name='mobile'/>\
-        		</p>\
-        		<p>\
-           			<button id='check'>检查表单</button>\
-        		</p>\
-        		<p>\
-           			<input type='submit' value='登录'/>\
-       			</p>\
-			</form></div>";
-			document.body.appendChild(oSignup);
+		var wHeight=$(window).height();
+		var oMask ="<div id='mask'></div>";
+			$("body").append(oMask);
+            $("#mask").height(sHeight);
+            $("#mask").width(sWidth);
+
+			var ahtml="<div id='signup'>"+
+                     "<div class='signupCon'>"+
+			         "<div id='signupheader'>"+
+			         "<div id='close'>点击关闭</div>"+
+			         "</div>"+
+			         "<form id='demoForm'>"+
+    		         "<fieldset>"+
+        		     "<legend align='center'>用户注册</legend>"+
+        		     "<p id='info'></p>"+
+        		     "<p id='info2' style='display: none'>输入错误</p>"+      			      
+            	     "<p><label for='username'>用户名</label>"+
+            	     "<input type='text' id='username' name='username'/></p>"+       		      
+           			"<p><label for='password'>密码</label>"+
+           			"<input type='password' id='password' name='password'/></p>"+       		
+            		"<p><label for='confirm-password'>确认密码</label>"+
+            		"<input type='password' id='confirm-password' name='confirm-password'/></p>"+        		   
+            	    "<p><label for='email'>邮箱</label>"+
+            	    "<input type='email' id='email' name='email'/></p>"+      		
+            	    "<p><label for='text'>手机号码</label>"+
+            	    "<input type='text' id='mobile' name='mobile'/></p>"+        		
+           			"<p><button id='check'>检查表单</button></p>"+
+           			"<p><input type='submit' value='登录'/></p>"+
+			        "</form></div></div>";
+			$("body").append(ahtml);
             validator1();
-	//获取登陆框的宽和高
-	var dHeight=oSignup.offsetHeight;
-	var dWidth=oSignup.offsetWidth;
+
+	    //获取登陆框的宽和高
+	    var dHeight=$("#signup").outerHeight();
+	    var dWidth=$("#signup").outerWidth();
+
 		//设置登陆框的left和top
-		oSignup.style.left=sWidth/2-dWidth/2+"px";
-		oSignup.style.top=wHeight/2-dHeight/2+"px";
-	//点击关闭按钮
-	var oClose=document.getElementById("close");
-	
+		$("#signup").css("left",sWidth/2-dWidth/2+"px");
+		$("#signup").css("top",wHeight/2-dHeight/2+"px");
+
+	    //点击关闭按钮	
 		//点击登陆框以外的区域也可以关闭登陆框
-		oClose.onclick=oMask.onclick=function(){
-					document.body.removeChild(oSignup);
-					document.body.removeChild(oMask);
+		$("#close").click(function(){
+
+					$("#signup").remove();
+					$("#mask").remove();
+					});
+
+        $("#mask").click(function(){
+                    $("#signup").remove();
+                    $("#mask").remove();
+                    });
 					}
-					}
+                    
     function validator1(){
     	var validator;
         $.validator.setDefaults({
@@ -136,6 +126,6 @@ window.onload=function(){
       	var length = value.length;    
       		return this.optional(element) || (length == 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value));    
     	}, "请正确填写手机号码。");
-} 
-				
+    } 
+});				
 
